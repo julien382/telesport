540b3625824dbd855797fe6ac2ee923f92f7f315
julien382 <julhermain@gmail.com>
2024-01-10 15:15:31 +0100

    css linechart mobile

--- src/app/components/linechart/linechart.component.ts
+++ src/app/components/linechart/linechart.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 
 interface LineChartData {
-  name: string; series: {value:number; name: string}[]
+  name: string;
+  series: { value: number; name: string }[];
 }
 
 @Component({
@@ -9,7 +10,7 @@ interface LineChartData {
   template: `
     <ngx-charts-line-chart
       [results]="data"
-      [view]="[500, 300]"
+      [view]="chartView"
       [gradient]="false"
       [xAxis]="true"
       [yAxis]="true"
@@ -19,4 +20,18 @@ interface LineChartData {
 })
 export class LinechartComponent {
   @Input() data: LineChartData[] = [];
+  chartView: [number, number] = [500, 300];
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.adjustChartSize();
+  }
+
+  private adjustChartSize(): void {
+    // Détecter si la largeur de l'écran est inférieure à 600px
+    const isMobile = window.innerWidth < 600;
+
+    // Ajuster la taille du graphique en conséquence
+    this.chartView = isMobile ? [300, 200] : [500, 300];
+  }
 }
